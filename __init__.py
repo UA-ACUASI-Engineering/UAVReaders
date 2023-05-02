db3433fbe36f68fee714ba44d2afa25c991484aa
Christian Clifford <cjclifford@alaska.edu>
2023-05-02 13:36:01 -0800

    More cleanup of what the module makes visible

--- __init__.py
+++ __init__.py
@@ -6,9 +6,9 @@ Contributors: Christian Clifford cjclifford1669@gmail.com
 collection of parsers for UAV related formats
 """
 
-from glob import glob
-import sys
-import os.path
+from glob import glob as _glob
+import sys  as _sys
+import os.path as _path
 
 
 class _add_path():
@@ -22,13 +22,13 @@ class _add_path():
 
     def __enter__(self):
         for p in self.path:
-            sys.path.insert(0, p)
+            _sys.path.insert(0, p)
             
 
     def __exit__(self, exc_type, exc_value, traceback):
         try:
             for p in self.path:
-                sys.path.remove(p)
+                _sys.path.remove(p)
 
         except ValueError:
             pass
@@ -36,8 +36,8 @@ class _add_path():
 def _import_uavreaders():
     global mavlink_reader
     global data_flash_reader
-    directory = os.path.dirname(__file__)
-    library_directories = glob(directory+"/build/lib*")
+    directory = _path.dirname(__file__)
+    library_directories = _glob(directory+"/build/lib*")
     with _add_path(library_directories):
         import UAVReaders as u
         mavlink_reader = u.mavlink_reader
