6d953df15704ef6b7942602fab79d525af191cd0
Christian Clifford <cjclifford@alaska.edu>
2025-04-16 10:48:28 -0600

    maybe a new gh action

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ ext_modules = [
 setup(
     ext_modules = cythonize(ext_modules),
     name="UAVReaders",
-    version="1.0.4",
+    version="1.0.5",
     author="Christian Clifford",
     author_email="cjclifford@alaska.edu",
     description="Parsers for UAV/Aerial Drone-related formats like MavLink and DataFlash",
